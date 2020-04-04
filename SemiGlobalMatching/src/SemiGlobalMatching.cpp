a06ac35d241d67bc3f8ef523ce8b62c0237c82f5
Liu Yang <ly_0913@foxmail.com>
2020-04-04 14:35:12 +0800

    add SemiGlobalMatching

---
+++ SemiGlobalMatching/src/SemiGlobalMatching.cpp
@@ -0,0 +1,14 @@
+﻿#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
