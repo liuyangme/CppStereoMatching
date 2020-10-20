e3db9cb387a7ca61273694405900f7d5ca41518d
Liu Yang <ly_0913@foxmail.com>
2020-10-20 21:47:02 +0800

    update

--- DepthOverlay/DepthOverlay.cpp
+++ DepthOverlay/DepthOverlay.cpp
@@ -9,5 +9,22 @@ using namespace cv;
 
 int main()
 {
+    Mat SrcImage = imread("srcImage/000117_10.png", 1);
+    Mat DepthImage = imread("srcImage/000117_10_res.png", 1);
+
+    if (SrcImage.empty() == 1 || DepthImage.empty() == 1) {
+        printf("Image Input Error!!");
+        return -1;
+    }
+
+    Mat ColorRes, OverLapping;
+
+    //伪彩色化
+    applyColorMap(DepthImage, ColorRes, COLORMAP_HSV);
+    //叠加
+    addWeighted(ColorRes, 0.4, SrcImage, 1, 0, OverLapping);
+
+    imwrite("resImage/000117_10.png", OverLapping);
+
     return 0;
 }
