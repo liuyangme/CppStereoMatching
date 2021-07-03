23dc29f6d7e77fc6639519b6cc38f295ca9bf5cf
Liu Yang <ly_0913@foxmail.com>
2021-07-03 14:10:16 +0800

    update

--- CostAggregationMatching/src/CostAggregationMatching.cpp
+++ CostAggregationMatching/src/CostAggregationMatching.cpp
@@ -62,8 +62,8 @@ int main(int argc, char** argv)
 			//Mat left_image = imread("images/" + name[i] + "/view1.png", 0);
 			//Mat right_image = imread("images/" + name[i] + "/view5.png", 0);
 
-			Mat left_image = imread("images/reL.bmp", 0);
-			Mat right_image = imread("images/reR.bmp", 0);
+			Mat left_image = imread("images/reL24.bmp", 0);
+			Mat right_image = imread("images/reR24.bmp", 0);
 
 			if ((!left_image.data) || (!right_image.data)) {
 				printf("Please input vaild image!\n");
@@ -80,18 +80,18 @@ int main(int argc, char** argv)
 			unsigned char* LAB_left_pnt, * LAB_right_pnt;
 			//RGB_left_image = imread("images/" + name[i] + "/view1.png", 1);
 			//RGB_right_image = imread("images/" + name[i] + "/view5.png", 1);
-			RGB_left_image = imread("images/reL.bmp", 1);
-			RGB_right_image = imread("images/reR.bmp", 1);
+			RGB_left_image = imread("images/reL24.bmp", 1);
+			RGB_right_image = imread("images/reR24.bmp", 1);
 			RGB_left_pnt = RGB_left_image.data;
 			RGB_right_pnt = RGB_right_image.data;
 			int rdim = left_image.rows;
 			int cdim = left_image.cols;
 
 			//匹配参数选择
-			int dispRange = 128;//disparity range
+			int dispRange = 256;//disparity range
 			int costChoice1 = 3;//(1) AD, (2) Rank, (3) Census, (4) LoG 
-			int costChoice2 = 1;//(0) Single Cost (1) AD, (2) Rank, (3) Census (4) LoG
-			int aggrChoice = 3;//(1) Block, (2) Adaptive Weight, (3) Cross-Based
+			int costChoice2 = 0;//(0) Single Cost (1) AD, (2) Rank, (3) Census (4) LoG
+			int aggrChoice = 1;//(1) Block, (2) Adaptive Weight, (3) Cross-Based
 			int autoScaleOrNot = 1;//1:Yes
 			float lambdaFirst = 1;
 			float lambdaSecond = 1;
@@ -99,7 +99,7 @@ int main(int argc, char** argv)
 			int mask_Rad = 1;
 			//Census Mask Initialization
 			int maskHrad = 5;
-			int maskWrad = 3;
+			int maskWrad = 7;
 			int maskT = (2 * maskHrad + 1) * (2 * maskWrad + 1);
 			//Rank Mask Initialization
 			int maskRankSize = 7;
@@ -291,8 +291,8 @@ int main(int argc, char** argv)
 			//imwrite("resultImages/" + name[i] + "/" + name[i] + "_disp1_L.png", show_image_color_L);
 			//imwrite("resultImages/" + name[i] + "/" + name[i] + "_disp5_R.png", show_image_color_R);
 			//cout << "Matching " + name[i] + " is OK!\n\r";
-			imwrite("resultImages/L.bmp", show_image_L);
-			imwrite("resultImages/R.bmp", show_image_R);
+			imwrite("resultImages/L24.bmp", show_image_L);
+			imwrite("resultImages/R24.bmp", show_image_R);
 
 			//视差评估
 			//Mat standardLeft = imread("images/" + name[i] + "/disp1.png", -1);
